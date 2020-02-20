d785b4e442b7cbfacbe86b93547bc4fa13ac0793
yinxiantao <yxt20000827>
2020-02-20 21:37:56 +0800

    第一次提交完成商品模块

--- mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 @SpringBootApplication
+
 public class MallAdminApplication {
     public static void main(String[] args) {
         /////////////////
