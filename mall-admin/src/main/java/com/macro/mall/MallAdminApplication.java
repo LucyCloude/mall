b7a2bfaf431c6e9528204c129a103f6570710555
ԬÖÐÍô <1831883242@qq.com>
2020-02-20 20:51:08 +0800

    完成商品模块

--- mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class MallAdminApplication {
     public static void main(String[] args) {
-        //////////////////////////ssssss/dfwfwfwfwedgaegreafgsrt
+        //////红红火火恍
         SpringApplication.run(MallAdminApplication.class, args);
     }
 }
